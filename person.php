588966b911da7e2a9505ec570fa3e9c368cd6f2b
Zhipeng Liu <hustlzp@qq.com>
2012-08-12 13:38:55 +0800

    对个人主页进行了重构

--- person.php
+++ person.php
@@ -7,67 +7,63 @@
 
 <div id='person-page'>
 	<!-- 用户信息 -->
-	<div id='user-info'>
+	<div id='user-info-panel'>
 		<img src='./imgs/gravatar-140.png' />
 		<span id='user-name'> <?php echo get_username_by_id($userID); ?> </span>
 	</div>
 	
-	<!-- 用户的 Goals -->	
-	<div class='goal-wap'>
-		<?php $nows = get_goals($userID, 'now'); ?>
-		<p class='goal-wap-header'>进行中 [<?php echo count($nows); ?>]</p>
-	
+	<!-- 用户的 Goals -->
+	<div id='goals-panel'>
 		<?php
-		foreach($nows as $now){
-			echo "<div class='goal-item goal-item-now'>"
-					."<p class='goal-title'>". $now['Title']. "</p>"
-					."<p class='goal-reason'>". $now['Reason']. "</p>"
-				. "</div>";
+		function goal_html_output($userID, $type){
+			$goals = get_goals($userID, $type);
+			foreach($goals as $goal){
+				echo "<div class='goal-item goal-item-". $type ."'>"
+						."<p class='goal-title'><a href='goal_page_details.php?goalID=". $goal['GoalID']. "'>". $goal['Title']. "</a></p>"
+						."<p class='goal-reason'>". $goal['Reason']. "</p>"
+					. "</div>";
+			}
 		} ?>
+		
+		<ul id='goal-wap-header'>
+			<li><a href='person.php?userID=<?php echo $userID ?>&goalType=now'>进行中 [<?php echo get_goal_num($userID, 'now'); ?>]</a></li
+			><li><a href='person.php?userID=<?php echo $userID ?>&goalType=future'>待启动 [<?php echo get_goal_num($userID, 'future'); ?>]</a></li
+			><li><a href='person.php?userID=<?php echo $userID ?>&goalType=finish'>已完成 [<?php echo get_goal_num($userID, 'finish'); ?>]</a></li>
+		</ul>
+		
+		<div class='goal-wap'>
+			<?php 
+			$goalType = isset($_REQUEST['goalType'])? $_REQUEST['goalType']: 'now';		
+			goal_html_output($userID, $goalType) ?>
+		</div>
+		
+		<!--
+		<div class='goal-wap'>
+			<?php goal_html_output($userID, 'future') ?>
+		</div>
+		
+		<div class='goal-wap'>
+			<?php goal_html_output($userID, 'finish') ?>
+		</div>
+		-->		
 	</div>
-	
-	<div class='goal-wap'>
-		<?php $futures = get_goals($userID, 'future'); ?>
-		<p class='goal-wap-header'>待启动 [<?php echo count($futures); ?>]</p>
 
-		<?php
-		foreach($futures as $future){
-			echo "<div class='goal-item goal-item-future'>"
-					."<p class='goal-title'>". $future['Title']. "</p>"
-					."<p class='goal-reason'>". $future['Reason']. "</p>"
-				. "</div>";
-		} ?>
-	</div>
-	
-	<div class='goal-wap'>
-		<?php $finishs = get_goals($userID, 'finish'); ?>
-		<p class='goal-wap-header'>已完成 [<?php echo count($finishs); ?>]</p>
-
-		<?php
-		foreach($finishs as $finish){
-			echo "<div class='goal-item goal-item-finish'>"
-					."<p class='goal-title'>". $finish['Title']. "</p>"
-					."<p class='goal-reason'>". $finish['Reason']. "</p>"
-				. "</div>";
-		} ?>
-	</div>
-	
 	<!-- 用户的个人动态 -->	
-	<div id="personal-dynamics">
-		<?php $dynamics = get_all_logs($userID) ?>
+	<div id="personal-dynamics-panel">
 		<p id='dynamic-header'>个人动态</p>
 		
 		<?php
+		$dynamics = get_all_logs($userID);
 		foreach($dynamics as $dyn){
 			echo "<div class='dynamic-item'>"
-				. "<p class='dynamic-goal-title'>在 <b>". $dyn['Title']. "</b> 中写道:"
+				. "<p class='dynamic-goal-title'>在 <b>". $dyn['Title']. "</b> 中写道:</p>"
 				. "<p class='dynamic-content'>". $dyn['LogContent']. "</p>"
 				. "<p class='dynamic-time'>". $dyn['LogTime']. "</p>"
 				. "</div>";
 		}
 		?>
 	</div>
-
+</div>
 <?php
 	require('footer.php');
 ?>
