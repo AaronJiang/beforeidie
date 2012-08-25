ff51231c0fd60808ffc053ea716e72ab85fc7d6d
Zhipeng Liu <hustlzp@qq.com>
2012-08-25 23:11:55 +0800

    动态的展示
    初步实现了对动态的展示，但是还是要通过优化样式，来突出不同动态！

--- dynamic_funs.php
+++ dynamic_funs.php
@@ -3,8 +3,8 @@
 	
 	//获取某 User 所关注的人的所有动态
 	function get_dynamics($userID){
-		//获取 Goal 的创建动态
-		$query = "(SELECT 'newGoal' as type, users.Username as Poster, goals.UserID as PoserID, GoalID, Title as GoalTitle, Reason as GoalReason, NULL as LogTitle, NULL as LogContent, CreateTime as Time\n"
+		//获取 Goal 相关的动态
+		$query = "(SELECT 'newGoal' as type, users.Username as Poster, goals.UserID as PosterID, GoalID, Title as GoalTitle, Reason as GoalReason, NULL as LogTitle, NULL as LogContent, CreateTime as Time\n"
 				. "FROM goals, users\n"
 				. "WHERE goals.UserID = users.UserID\n"
 				. "AND goals.UserID IN\n"
@@ -13,7 +13,7 @@
 					. "WHERE followers.FollowerID =". $userID. ")\n"
 				. ")\n";
 		$query .= "UNION ALL\n";
-		//获取 Log 动态
+		//获取 Log 相关的动态
 		$query .= "(SELECT 'newLog' as type, users.Username as Poster, goals.UserID as PosterID, goals.GoalID, goals.Title as GoalTitle, NULL as GoalReason, logs.LogTitle, logs.LogContent, logs.LogTime as Time\n"
 				. "FROM goal_logs as logs, goals, users\n"
 				. "WHERE logs.GoalID = goals.GoalID\n"
