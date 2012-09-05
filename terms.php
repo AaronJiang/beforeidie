006699ce32c6b0547866e427f55c1d35eaedc6b4
Zhipeng Liu <hustlzp@qq.com>
2012-09-05 20:55:04 +0800

    提取about_me页边栏

--- terms.php
+++ terms.php
@@ -7,7 +7,7 @@
 	}
 ?>
 <script type="text/javascript">
-	$('body').prop('id', 'page-about');
+	$('body').prop('id', 'page-about-terms');
 </script>
 
 <div id='main-content'>
@@ -49,22 +49,7 @@
 	</ul>
 </div>
 
-<div id='sidebar'>
-	<p class="main-title">关于我</p>
-	
-	<img id='my-avatar' src='imgs/me.jpg' />
-	
-	<div id='my-intro'>
-		<p>lzp，90后吊丝一枚，2012年毕业于关山口职业技术学院。对本专业完全无爱，考研遂转投计算机阵营，方向机器视觉，业余爱好Web开发。</p>
-		<p><b>E-mail :</b> hustlzp@qq.com</p>
-	</div>
-	
-	<p class='main-title'>付费支持</p>
-	
-	<p>Goal的运营费用是从个人生活费中挤出，资金相当有限，欢迎付费支持！</p>
-	<span id='donate-btn' href=''>Donate</span>
-</div>
-
 <?php
+	require('about_me.php');
 	require('footer.php');
 ?>
