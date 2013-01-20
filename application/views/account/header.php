e754cd871dffcba54ba879978f691f468e5c0d33
hustlzp <hustlzp@qq.com>
2013-01-20 22:12:46 +0800

    rm project files & imgs not use

--- application/views/account/header.php
+++ application/views/account/header.php
@@ -3,19 +3,9 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<title><?= $pageTitle ?></title>
-
-		<!-- jQuery & jQuery UI -->
-		<link rel="stylesheet" href="<?= base_url('static/style/jquery-ui-1.8.22.custom.css') ?>" />
 		<script type="text/javascript" src="<?= base_url('static/js/jquery-1.7.2.min.js') ?>"></script>
 		<script type="text/javascript" src="<?= base_url('static/js/jquery-ui-1.8.22.custom.min.js') ?>"></script>
-
-		<!-- css -->
 		<link rel="stylesheet" href="<?= base_url('static/style/style.css') ?>" />
-
-		<!-- validation engine -->
-		<script type="text/javascript" src="<?= base_url('static/js/jquery.validationEngine-zh_CN.js') ?>"></script>
-		<script type="text/javascript" src="<?= base_url('static/js/jquery.validationEngine.js') ?>"></script>
-		<link rel="stylesheet" href="<?= base_url('static/style/validationEngine.jquery.css') ?>" type="text/css"/>
 	</head>
 
 	<body id="<?= $pageID ?>">
--- application/views/header.php
+++ application/views/header.php
@@ -3,21 +3,9 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<title><?= $pageTitle ?></title>
-
-		<!-- jQuery & jQuery UI -->
 		<script type="text/javascript" src="<?= base_url('static/js/jquery-1.7.2.min.js') ?>"></script>
 		<script type="text/javascript" src="<?= base_url('static/js/jquery-ui-1.8.22.custom.min.js') ?>"></script>
-		<link rel="stylesheet" href="<?= base_url('static/style/jquery-ui-1.8.22.custom.css') ?>" />
-		
-		<!-- validation engine -->
-		<script type="text/javascript" src="<?= base_url('static/js/jquery.validationEngine-zh_CN.js') ?>"></script>
-		<script type="text/javascript" src="<?= base_url('static/js/jquery.validationEngine.js') ?>"></script>
-		<link rel="stylesheet" href="<?= base_url('static/style/validationEngine.jquery.css') ?>" type="text/css" />
-
-		<!-- feedback js -->
 		<script type="text/javascript" src="<?= base_url('static/js/goal-feedback.js') ?>"></script>
-
-		<!-- less css -->
 		<link rel="stylesheet" href="<?= base_url('static/style/style.css') ?>" />
 	</head>
 
