03aa85d6eb769dcc0c32da566001c86aa2842c26
hustlzp <hustlzp@qq.com>
2013-01-25 09:51:05 +0800

    update footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -2,6 +2,7 @@
 	
 		<div id='footer'>
 			<a href="<?= base_url('about') ?>">关于</a>
+			<a href="https://github.com/hustlzp/beforeidie" target="_blank">GitHub</a>
 			<span>2012 © beforeidie.asia</span>
 		</div>
 	</body>
